e8047f4b8aeaf188b2165878fd91b4cdd92db3f5
Zachary Liu <liu_zachary@live.com>
2018-12-12 23:39:10 -0500

    commit before pull

--- app.py
+++ app.py
@@ -20,6 +20,11 @@ connect("mongodb://daequan:360oogabooga@ds119151.mlab.com:19151/bme590finaldb")
 
 
 def detectFname(path):
+    """
+    Parses filepath for the filename
+
+    :returns filename
+    """
     loc = path.rfind("/")
     if(loc == -1):
         return path
@@ -28,22 +33,36 @@ def detectFname(path):
 
 
 def detectFilePathNoName(path):
+    """
+    Parses filepath for the filepath without the file name ending
+
+    :returns path string
+    """
     loc = path.rfind("/")
     if(loc == -1):
-        return ""
+        return loc
     else:
         return path[0:loc+1]
 
 
 def detectFtype(path):
+    """
+    Parses filepath for the type of file
+
+    :returns filetype
+    """
     loc = path.rfind(".")
     if(loc == -1):
-        print("youre fucked")
         return loc
     else:
         return path[loc:]
 
 def getRawName(path):
+    """
+    Given a filename with no path before it, returns just the name, no type
+
+    :returns filename
+    """
     loc = path.rfind(".")
     if loc == -1:
         return loc
@@ -51,6 +70,9 @@ def getRawName(path):
         return path[0:loc]
 
 def constructImg(uploadTime, fileName, imgString, fileType, dimensions):
+    """
+    Creates Image Struct
+    """
     toret = {}
     toret['uploadtime'] = uploadTime
     toret['filename'] = fileName
@@ -62,6 +84,11 @@ def constructImg(uploadTime, fileName, imgString, fileType, dimensions):
 
 
 def encode_image_as_b64_wmetrics(image_path):
+    """
+    Encodes imagepath to 64b with metrics like name, type, and dimensions
+
+    :returns list of metrics as well as encoding
+    """
     imname = detectFname(image_path)
     img = np.asarray(Image.open(image_path))
     shape = img.shape
@@ -69,13 +96,22 @@ def encode_image_as_b64_wmetrics(image_path):
     return [imname, imtype, shape, base64.b64encode(img).decode()]
 
 
+
 def decode_b64_image_helper(base64_string, format, dimensions):
+    """
+    Helper Function for decode
+    """
     decode = base64.b64decode(base64_string)
     res = np.frombuffer(decode, dtype=np.uint8)
     return np.reshape(res, dimensions)
 
 
 def decode_image_fromb64(imstring, format, shape):
+    """
+    Decodes image string into numpy image
+
+    :returns nd array image of dtype uint8
+    """
     imbytes = imstring.encode()
     decoded = decode_b64_image_helper(imbytes, format, shape)
     return decoded
@@ -95,17 +131,16 @@ def validateNewUser(input):
 def upload_image(user, filename, filetype, dimensions, filedata):
     """
     Needs fields filename, filedata, username, filetype, dimensions
+
+    Constructs and then uploads image to appropriate user obj in db
+
+    :returns 1 for funsies
     """
-    # img = Image(datetime.datetime.now(),
-    #             r['filename'],
-    #             r['filedata'])
     newim = constructImg(datetime.datetime.now(),
                         filename,
                         filedata,
                         filetype,
                         dimensions)
-    # if len(User.objects.({"_id": user}))==0:
-    #    createUser(user)
     user_call = User.objects.raw({"_id": user}).first()
     counter = 0
     for k in user_call.imgslist:
@@ -120,39 +155,87 @@ def upload_image(user, filename, filetype, dimensions, filedata):
 
 
 def showIM(username, filename):
+    """
+    Displays image as specified in filename
+
+    :returns doesn't return lul
+    """
     user_call = User.objects.raw({"_id": username}).first()
+    saveupload = ""
+    saveshape = []
     for k in user_call.imgslist:
         if k == "":
             continue
         if k['filename'] == filename:
             #imdata = k['imgstring']
             decodedim = decode_image_fromb64(k['imgstring'], k['filetype'], k['dimensions'])
+            saveshape = str(k['dimensions'])
+            saveupload = k['uploadtime'].strftime("%Y-%m-%d %H:%M:%S")
+    if saveupload == "":
+        return -1
     plt.imshow(decodedim)
+    plt.title(saveupload + "  " + saveshape)
     plt.show()
 
+
 def downloadIM(username, filename, fileformat):
+    """
+    Downloads specified image in given format
+
+    Idk what to do for zip lol
+    """
     testcount = User.objects.raw({"_id": username}).count()
     if testcount == 0:
         return
     user_call = User.objects.raw({"_id": username}).first()
+    save = ''
     for k in user_call.imgslist:
         if k == "":
             continue
         if k['filename'] == filename:
             #imdata = k['imgstring']
+            save = k['imgstring']
             decodedim = decode_image_fromb64(k['imgstring'], k['filetype'], k['dimensions'])
-    plt.imshow(decodedim)
-    plt.show()
+    #imbytes = save.encode()
+    #decode = base64.b64decode(imbytes)
+    #plt.imshow(decodedim)
+    #plt.show()
     tosave = Image.fromarray(decodedim)
     if fileformat == ".jpg":
         tosave = tosave.convert("RGB")
     savestring = getRawName(filename) + datetime.datetime.now().strftime("%Y%m%d%H%M%S") + fileformat
     #print(savestring)
+    #f = io.BytesIO()
     tosave.save(savestring)
+    return 1;
+    #with zipfile.ZipFile('spam.zip', 'w') as myzip:
+    #    myzip.writestr(savestring, tosave.tobytes())
+
+
+def showHI(username, filename):
+    """
+    Displays histogram of given image
+    """
+    user_call = User.objects.raw({"_id": username}).first()
+    saveupload = ""
+    saveshape = []
+    onedim = ''
+    for k in user_call.imgslist:
+        if k == "":
+            continue
+        if k['filename'] == filename:
+            #imdata = k['imgstring']
+            decodedim = decode_image_fromb64(k['imgstring'], k['filetype'], k['dimensions'])
+            onedim = k['dimensions'][0]
+    plt.hist(np.reshape(decodedim, (-1,1)), bins = 256, range=(0.0, 255.0))
+    plt.title("histogram")
+    plt.show()
+
 
 class User(MongoModel):
     username = fields.CharField(primary_key=True)
     imgslist = fields.ListField()
+    loginhist = fields.ListField()
 
 
 class App(QMainWindow):
@@ -219,12 +302,16 @@ class App(QMainWindow):
         self.button7=QPushButton('Save Image',self)
         self.button7.move(400,600)
         self.button7.clicked.connect(self.downloadImage)
+        self.button8=QPushButton('Show Histogram',self)
+        self.button8.move(30,700)
+        self.button8.clicked.connect(self.showHistogram)
         self.button.resize(200,30)
         self.button2.resize(200,30)
         self.button3.resize(200,30)
         self.button4.resize(200,30)
         self.button5.resize(200,30)
         self.button7.resize(200,30)
+        self.button8.resize(200,30)
         #self.imlabel = Qlabel(self)
 
         self.show()
@@ -238,12 +325,13 @@ class App(QMainWindow):
         if(check == 0):
            msg =  "User Set (Already Exists)"
            self.user = username
+           User.object.raw({"_id":username}).First().loginhist.append(datetime.datetime.now())
         if check==1:
-            u = User(username=username, imgslist=[""])
+            u = User(username=username, imgslist=[""], loginhist = [datetime.datetime.now()])
             u.save()
             msg =  "User Created"
             self.user = username
-        QMessageBox.question(self, 'Hello, world!',msg, QMessageBox.Ok, QMessageBox.Ok)
+        QMessageBox.question(self, 'Message!',msg, QMessageBox.Ok, QMessageBox.Ok)
 
     def callAddImage(self):
         fileName, _ = QFileDialog.getOpenFileNames(self, 'Single File', QtCore.QDir.rootPath() , '*.jpg;*.png;*.tif;*.zip')
@@ -275,25 +363,15 @@ class App(QMainWindow):
         showIM(self.user, self.textbox2.text())
         #r= requests.post("http://127.0.0.1:5000/api/upload_image", json={"username":"chris", "filename":fname})
 
+    def showHistogram(self):
+        showHI(self.user, self.textbox2.text())
+
     def callExternalProcessing(self):
         print(self.comboBox.currentText())
         #r= requests.post("http://127.0.0.1:5000/api/improcessing", json={"username":"chris", "filename":"cat.jpg", "processing": "histogram equalization"})
 
     def on_click(self):
-        #textboxValue=self.textbox.text()
-        #QMessageBox.question(self, 'Hello, world!', "Confirm: "+textboxValue,QMessageBox.Ok, QMessageBox.Ok)
-        ret, _ = QFileDialog.getOpenFileNames(self, 'Single File', QtCore.QDir.rootPath() , '*.jpg;*.png;*.tif;*.zip')
-        #QMessageBox.question(self, 'Hello, world!', "Confirm: "+fileName,QMessageBox.Ok, QMessageBox.Ok)
-        prepend = 0
-        for k in ret:
-            prepend = prepend + 1
-            mypath = detectFilePathNoName(k)
-            ndir = mypath + str(prepend) + datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-            with zipfile.ZipFile(k,"r") as zip_ref:
-                zip_ref.extractall(ndir)
-            for j in os.listdir(ndir):
-                nfile =mypath + j
-                self.files.append(nfile)
+        print("nada")
 
 if __name__ == '__main__':
     app=QApplication(sys.argv)
