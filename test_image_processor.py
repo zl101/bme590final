33213cc57395aefb3d54b8beaa26213d110269e4
eh158 <eh158@duke.edu>
2018-12-14 16:26:33 -0500

    Added skeleton file for test functions

---
+++ test_image_processor.py
@@ -0,0 +1,58 @@
+import pytest
+from image_processor import *
+
+
+@pytest.mark.parametrize("filename, expected, detected", [
+])
+def test_ecode_b64_image_helper(args, expected, detected){
+
+}
+
+
+@pytest.mark.parametrize("filename, expected, detected", [
+])
+def test_decode_image_from_b64(args, expected, detected){
+
+}
+
+
+@pytest.mark.parametrize("filename, expected, detected", [
+])
+def test_contrast_stretch(args, expected, detected){
+
+}
+
+
+@pytest.mark.parametrize("filename, expected, detected", [
+])
+def test_log_compress(args, expected, detected){
+
+}
+
+
+@pytest.mark.parametrize("filename, expected, detected", [
+])
+def test_reverse_video(args, expected, detected){
+
+}
+
+
+@pytest.mark.parametrize("filename, expected, detected", [
+])
+def test_gamma_correct(args, expected, detected){
+
+}
+
+
+@pytest.mark.parametrize("filename, expected, detected", [
+])
+def test_validateNewUser(args, expected, detected){
+
+}
+
+
+@pytest.mark.parametrize("filename, expected, detected", [
+])
+def test_ValidateNewImage(args, expected, detected){
+
+}
